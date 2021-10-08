e57519c8eea0fdfa9909889357f59f0c781b42bc
Albert Moravec <albert.moravec@gmail.com>
2021-10-09 00:28:14 +0200

    Add breadcrumbs on user page

--- assets/src/routes/Users.svelte
+++ assets/src/routes/Users.svelte
@@ -1,14 +1,24 @@
 <script>
   import { _ } from "svelte-i18n";
-  import { PageHeader } from "svelte-adminlte";
+  import { PageHeader, BreadcrumbItem } from "svelte-adminlte";
   import UserDetail from "../controls/users/UserDetail.svelte";
   import UserList from "../controls/users/UserList.svelte";
   import UserOrders from "../controls/users/UserOrders.svelte";
+  import usersChannel from "../providers/socket/usersChannel";
 
   let selectedUserId = null;
 </script>
 
-<PageHeader>{$_("users.title")} <small>Twin Peaks Community</small></PageHeader>
+<PageHeader>
+  <svelte:fragment>
+    {$_("users.title")} <small>Twin Peaks Community</small>
+  </svelte:fragment>
+
+  <svelte:fragment slot="breadcrumbs">
+    <BreadcrumbItem><a href="#/">{$_("home.title")}</a></BreadcrumbItem>
+    <BreadcrumbItem active>{$_("users.title")}</BreadcrumbItem>
+  </svelte:fragment>
+</PageHeader>
 
 <div class="row">
   <div class:col-12={!selectedUserId} class:col-6={selectedUserId}>

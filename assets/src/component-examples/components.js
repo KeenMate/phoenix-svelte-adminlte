09b8f4a3a41de7ac7ad870e7dfcfc1ea2fab0433
ZelvaMan <zelvaman16@gmail.com>
2021-11-26 22:44:36 +0100

    added tabs to components

--- assets/src/component-examples/components.js
+++ assets/src/component-examples/components.js
@@ -4,7 +4,9 @@ import TextInputExample from "./TextInputExample.svelte"
 
 
 export const components = [
-    {name:"Card", code:"card", component: CardExample, description:"Make everything look better"},
-    {name:"TextInput",code:"text-input", component: TextInputExample, description:"text input made into svelte component"}
+    {name:"Card", code:"card", component: CardExample,category: "structure", description:"Make everything look better"},
+    {name:"TextInput",code:"text-input", component: TextInputExample, category:"input", description:"text input made into svelte component"}
 
 ] 
+
+export const categories = ["structure","input"]
--- assets/src/controls/Components/ComponentList.svelte
+++ assets/src/controls/Components/ComponentList.svelte
@@ -11,32 +11,46 @@
     FormGroup,
     InputGroup,
     InputGroupAppend,
-    Pagination,
+    Tabs,
+    TabItem
   } from "svelte-adminlte";
 
-  import { components } from "../../component-examples/components";
-import { replace } from "svelte-spa-router";
+  import { components ,categories} from "../../component-examples/components";
+
+import { push } from "svelte-spa-router";
 import {fillParams} from "../../routes"
 import { _ } from "svelte-i18n";
+import { claim_component } from "svelte/internal";
 
   const dispatch = createEventDispatcher();
 
-  export let displayedComponents = [];
+  export let displayedComponents = components
+  export let category=null;
 
   let searchInput = null;
-  let searchDebounce = debounce(() => getComponents(), TypingDebounceDelay);
+  let searchDebounce = debounce(() => getComponents(category), TypingDebounceDelay);
 
   $: searchDebounce(searchInput);
   $: console.log(displayedComponents)
-  getComponents();
+  $: console.log(category)
+  
+  
 
   function getComponents() {
       if(searchInput == null || searchInput == ""){
-        displayedComponents = components;
+        displayedComponents = components.filter((x)=> (category === null?true : x.category == category ));
       }else{
-        console.log(`searching components ${searchInput}`)
-        displayedComponents = displayedComponents.filter((x)=>  x.name.toLowerCase().includes(searchInput.toLowerCase()) );
+        console.log(`searching components text:${searchInput}`)
+        displayedComponents = components.filter((x)=>  x.name.toLowerCase().includes(searchInput.toLowerCase())  );
       }
+  }
+
+  function changeCategory (cat){
+    if(cat !== category){
+    searchInput = null;
+    category = cat;
+    getComponents();
+    }
   }
 </script>
 
@@ -60,7 +74,17 @@ import { _ } from "svelte-i18n";
       </Form>
     </div>
   </div>
-
+<div class="row">
+  <div class="col-12">
+    <Tabs>
+      <li class="pt-2 px-3"><h3 class="card-title">{$_("component-list.categories")}</h3></li>
+      <TabItem active={category === null} on:click={() => changeCategory(null)}>{$_("component-list.all")}</TabItem>
+      {#each categories as c}
+        <TabItem active={category === c } on:click={() => changeCategory(c)}>{c}</TabItem>
+      {/each}
+    </Tabs>
+  </div>
+</div>
   <div class="row">
     <div class="col-12">
       <TableCondensed class="components-list">
@@ -74,7 +98,7 @@ import { _ } from "svelte-i18n";
             <td class="title">
               <a
                 href="#"
-                on:click|preventDefault={() => replace(fillParams("/components/show/:code",{code:component.code}))}
+                on:click|preventDefault={() => push(fillParams("/components/show/:code",{code:component.code}))}
               >
                 {component.name}
               </a>
@@ -101,16 +125,15 @@ import { _ } from "svelte-i18n";
   :global {
     .components-list {
       display: grid;
-      grid-template-columns: auto repeat(3, 1fr);
-
-      &:not(.expanded) {
-        grid-template-columns: auto 1fr;
-      }
+      grid-template-columns: minmax(5em,10em) auto;
 
       thead,
       tbody,
       * > tr {
         display: contents;
+      }
+      tr:first-child {
+        border-top: none;
       }
     }
   }

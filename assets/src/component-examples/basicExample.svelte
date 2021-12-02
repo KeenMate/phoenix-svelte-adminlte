2395076be4d4035cd6f0da99a59cfccf68e7babb
ZelvaMan <zelvaman16@gmail.com>
2021-12-02 18:26:21 +0100

    added DateRangePickerExample

---
+++ assets/src/component-examples/basicExample.svelte
@@ -0,0 +1,42 @@
+<script>
+    import ComponentPageTemplate from "../controls/Components/ComponentPageTemplate.svelte";
+    import ComponentExampleTemplate from "../controls/Components/ComponentExampleTemplate.svelte";
+  
+  
+    let data = {
+      name: "",
+      text: "",
+      props: [
+        { name: "", type: "", comment: "" },
+        ],
+      events:[
+        {name:"",comment:"", params:""}
+      ],
+      examples: {
+        minimal: {
+          name: "",
+          code: '',
+        }
+        
+      },
+    };
+  
+    
+  </script>
+  
+  <ComponentPageTemplate
+    text={data.text}
+    properties={data.props}
+    events={data.events}
+  >
+    <svelte:fragment slot="examples">
+      <ComponentExampleTemplate
+        code={data.examples.minimal.code}
+        name={data.examples.minimal.name}
+      >
+        
+      </ComponentExampleTemplate>
+      
+    </svelte:fragment></ComponentPageTemplate
+  >
+  

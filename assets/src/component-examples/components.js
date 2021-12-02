44f2e6946f6926849027a544dcf2db5f43c81811
ZelvaMan <zelvaman16@gmail.com>
2021-12-02 17:34:35 +0100

    added CkEditorEzxample

--- assets/src/component-examples/components.js
+++ assets/src/component-examples/components.js
@@ -2,11 +2,14 @@
 import CardExample from "./CardExample.svelte";
 import TextInputExample from "./TextInputExample.svelte"
 import CheckboxExample from "./CheckboxExample.svelte"
+import CkEditorExample from "./CkEditorExample.svelte"
 
 export const components = [
     {name:"Card", code:"card", component: CardExample,category: "Structure", description:"Make everything look better"},
     {name:"TextInput",code:"text-input", component: TextInputExample, category:"Input", description:"Text input made into svelte component"},
-    {name:"Checkbox", code:"checkbox", component: CheckboxExample, category:"Input", description:"Checkbox input"}
+    {name:"Checkbox", code:"checkbox", component: CheckboxExample, category:"Input", description:"Checkbox input"},
+    {name:"CkEditor", code:"ckeditor", component: CkEditorExample, category:"Input", description:"Simple but rich text editor"},
+
 ] 
 
 export const categories = ["Structure","Input"]
--- assets/src/controls/Components/ComponentExampleTemplate.svelte
+++ assets/src/controls/Components/ComponentExampleTemplate.svelte
@@ -38,6 +38,8 @@
     margin: 10 20px
     .card-body
       padding: 0.75em
+    .row
+      min-height: 150px
   .highlighter
     background-color: #f5f5f5
     height: 100%

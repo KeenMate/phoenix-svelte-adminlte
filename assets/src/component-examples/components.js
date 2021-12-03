4460820e52c75a48bdfa26239f976427b30a687d
ZelvaMan <zelvaman16@gmail.com>
2021-12-03 22:08:56 +0100

    typo

--- assets/src/component-examples/components.js
+++ assets/src/component-examples/components.js
@@ -15,7 +15,7 @@ export const components = [
     {name:"CkEditor", code:"ckeditor", component: CkEditorExample, category:"Input", description:"Simple but rich text editor"},
     {name:"DateRangePicket", code:"date-range-example", component: DateRangePicketExample, category:"Input", description:"Date range picker using Litepicker"},
     //{name:"Dropdown", code:"dropdown", component: DropdownExample, category:"Input", description:"Select 2 Dropdown"},
-    {name:"EmailInputExample",code:"email-input-example",component: EmailInputExample, category:"Input",description:"Email Input"},
+    {name:"EmailInput",code:"email-input-example",component: EmailInputExample, category:"Input",description:"Email Input"},
     {name:"FileInput", code:"file-input", component: FileInputExample,category: "Input", description:"File input for uploading one or more files"},
 
 ] 

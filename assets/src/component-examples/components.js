c55fc0cd6348241fff08ea13e585142792f8acb3
ZelvaMan <zelvaman16@gmail.com>
2021-12-03 22:07:40 +0100

    added FileUploadExample

--- assets/src/component-examples/components.js
+++ assets/src/component-examples/components.js
@@ -6,6 +6,7 @@ import CkEditorExample from "./CkEditorExample.svelte"
 import DateRangePicketExample from "./DateRangePicketExample.svelte"
 //import DropdownExample from "./DropdownExample.svelte"
 import EmailInputExample from "./EmailInputExample.svelte"
+import FileInputExample from "./FileInputExample.svelte"
 
 export const components = [
     {name:"Card", code:"card", component: CardExample,category: "Structure", description:"Make everything look better"},
@@ -14,7 +15,9 @@ export const components = [
     {name:"CkEditor", code:"ckeditor", component: CkEditorExample, category:"Input", description:"Simple but rich text editor"},
     {name:"DateRangePicket", code:"date-range-example", component: DateRangePicketExample, category:"Input", description:"Date range picker using Litepicker"},
     //{name:"Dropdown", code:"dropdown", component: DropdownExample, category:"Input", description:"Select 2 Dropdown"},
-    {name:"EmailInputExample",code:"email-input-example",component: EmailInputExample, category:"Input",description:"Email Input"}
+    {name:"EmailInputExample",code:"email-input-example",component: EmailInputExample, category:"Input",description:"Email Input"},
+    {name:"FileInput", code:"file-input", component: FileInputExample,category: "Input", description:"File input for uploading one or more files"},
+
 ] 
 
 export const categories = ["Structure","Input"]

7fbaffdff36e53c676923131caeef6b9225b4e7a
ZelvaMan <zelvaman16@gmail.com>
2021-12-10 23:05:18 +0100

    added table of colors to switchexample

--- assets/src/component-examples/Input/SwitchExample.svelte
+++ assets/src/component-examples/Input/SwitchExample.svelte
@@ -1,8 +1,8 @@
 <script>
   import ComponentPageTemplate from "../../controls/Components/ComponentPageTemplate.svelte";
   import ComponentExampleTemplate from "../../controls/Components/ComponentExampleTemplate.svelte";
-
-  import { Switch, FormGroup } from "svelte-adminlte";
+  import notification from "../../providers/notificationProvider";
+  import { Switch, TableCondensed } from "svelte-adminlte";
   let data = {
     name: "Switch Example",
     text: "",
@@ -21,20 +21,38 @@
           "Use classes that add backgroud-color (bg-red ,bg-primary, atd.).",
       },
     ],
-    events: [{ name: "click", comment: "", params: "" }],
+    events: [
+      { name: "change", comment: "", params: "" },
+      { name: "checked", comment: "value changed to true", params: "" },
+      { name: "unchecked", comment: "value changed to false", params: "" },
+    ],
     examples: {
       minimal: {
         name: "usage",
-        code: "<Switch bind:value={checked} />",
+        code: "<Switch bind:checked1 \/>",
+      },
+      event: {
+        name: "Events ",
+        code: "<Switch\r\n  bind:checked2\r\n  checkedClass=\"bg-green\"\r\n  on:change={(e) => {\r\n    console.log(\"CHANGED\");\r\n    notify(\"changed\");\r\n  }}\r\n  on:checked={(e) => {\r\n    notify(\"checked\");\r\n  }}\r\n  on:unchecked={(e) => {\r\n    notify(\"unchecked\");\r\n  }}\r\n\/>",
       },
       color: {
         name: "colors",
-        code: '<FormGroup>\r\n        bg-green<Switch  checkedClass="bg-green" uncheckedClass="bg-red" bind:checked={checked} />\r\n      </FormGroup>\r\n      <FormGroup>\r\n        bg-blue<Switch  checkedClass="bg-blue" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      </FormGroup>\r\n      <FormGroup>\r\n        bg-primary<Switch  checkedClass="bg-primary" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      </FormGroup>\r\n      <FormGroup>\r\n        bg-success<Switch  checkedClass="bg-success" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      </FormGroup>\r\n      <FormGroup>\r\n        bg-danger<Switch  checkedClass="bg-danger" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      </FormGroup>\r\n      <FormGroup>\r\n        bg-warning<Switch  checkedClass="bg-warning" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      </FormGroup>\r\n      <FormGroup>\r\n        bg-info<Switch  checkedClass="bg-info" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      </FormGroup>\r\n      <FormGroup>\r\n        bg-light<Switch  checkedClass="bg-light" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      \r\n      </FormGroup>\r\n      <FormGroup>\r\n          bg-dark<Switch  checkedClass="bg-dark" uncheckedClass="bg-gray" bind:checked={checked} />\r\n        </FormGroup>\r\n        <FormGroup>\r\n          bg-white<Switch  checkedClass="bg-white" uncheckedClass="bg-gray" bind:checked={checked} />\r\n      \r\n        </FormGroup> ',
+        code: '<TableCondensed>\r\n<svelte:fragment slot=\"headers\">\r\n  <tr>\r\n    <th>checked<\/th>\r\n    <th>unchecked<\/th>\r\n    <th>switch<\/th>\r\n  <\/tr>\r\n<\/svelte:fragment>\r\n{#each colors as color}\r\n  <tr>\r\n    <td>{color.f}<\/td>\r\n    <td>{color.s}<\/td>\r\n    <td>\r\n      <Switch checkedClass={color.f} \r\n      uncheckedClass={color.s} \/>\r\n\r\n    <\/td>\r\n  <\/tr>\r\n{\/each}\r\n<\/TableCondensed>',
       },
     },
   };
-
-  let checked;
+  function notify(text) {
+    console.log("NOTIFY");
+    notification.success(text);
+  }
+  let colors =  [
+    {f:"bg-primary",s:"bg-secondary"},
+    {f:"bg-success",s:"bg-black"},
+    {f:"bg-danger",s:"bg-dark"},
+    {f:"bg-warning  ",s:"bg-muted"},
+    {f:"bg-info",s:"bg-danger"},
+  ]
+  let checked1,checked2;
 </script>
 
 <ComponentPageTemplate
@@ -47,78 +65,51 @@
       code={data.examples.minimal.code}
       name={data.examples.minimal.name}
     >
-      <Switch bind:checked />
+      <Switch bind:checked1 />
+    </ComponentExampleTemplate>
+    <ComponentExampleTemplate
+      code={data.examples.event.code}
+      name={data.examples.event.name}
+    >
+<Switch
+  bind:checked2
+  checkedClass="bg-green"
+  on:change={(e) => {
+    console.log("CHANGED");
+    notify("changed");
+  }}
+  on:checked={(e) => {
+    notify("checked");
+  }}
+  on:unchecked={(e) => {
+    notify("unchecked");
+  }}
+/>
     </ComponentExampleTemplate>
     <ComponentExampleTemplate
       code={data.examples.color.code}
       name={data.examples.color.name}
     >
-      <FormGroup>
-        bg-green<Switch
-          checkedClass="bg-green"
-          uncheckedClass="bg-red"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-blue<Switch checkedClass="bg-blue" bind:checked />
-      </FormGroup>
-      <FormGroup>
-        bg-primary<Switch
-          checkedClass="bg-primary"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-success<Switch
-          checkedClass="bg-success"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-danger<Switch
-          checkedClass="bg-danger"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-warning<Switch
-          checkedClass="bg-warning"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-info<Switch
-          checkedClass="bg-info"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-light<Switch
-          checkedClass="bg-light"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-dark<Switch
-          checkedClass="bg-dark"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
-      <FormGroup>
-        bg-white<Switch
-          checkedClass="bg-white"
-          uncheckedClass="bg-gray"
-          bind:checked
-        />
-      </FormGroup>
+<TableCondensed>
+<svelte:fragment slot="headers">
+  <tr>
+    <th>checked</th>
+    <th>unchecked</th>
+    <th>switch</th>
+  </tr>
+</svelte:fragment>
+{#each colors as color}
+  <tr>
+    <td>{color.f}</td>
+    <td>{color.s}</td>
+    <td>
+      <Switch checkedClass={color.f} 
+      uncheckedClass={color.s} />
+
+    </td>
+  </tr>
+{/each}
+</TableCondensed>
     </ComponentExampleTemplate>
   </svelte:fragment>
 </ComponentPageTemplate>

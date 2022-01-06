8e405312b2e537477906b642f47672b8cc130cc9
Filip Jakab <jakabfilip.fj@gmail.com>
2022-01-06 02:30:10 +0100

    Some changes regarding LocaleDropdown.svelte

--- assets/src/App.svelte
+++ assets/src/App.svelte
@@ -1,189 +1,133 @@
 <script>
-  import { onMount, setContext } from "svelte";
-  import { get } from "svelte/store";
-  import Router from "svelte-spa-router";
-  import keymage from "keymage";
+  import {onMount, setContext} from "svelte"
+  import {get} from "svelte/store"
+  import Router from "svelte-spa-router"
+  import keymage from "keymage"
 
-  import "./locale/i18n";
-  import { changeLang, locale, languages, GetFlagPath } from "./locale/i18n";
-  import { _ } from "svelte-i18n";
-  import routes, { Routes } from "./routes";
-
-  import currentUser from "./stores/current-user";
-  import sidebarOpenState from "./stores/sidebar-open-state";
-  import {
-    login,
-    isAuthenticated,
-    userInfo,
-    AzureProvider,
-    ZuubrProvider,
-    appMountCallback,
-    logout,
-  } from "./stores/authentication";
+  import "./locale/i18n"
+  import {locale} from "./locale/i18n"
+  import routes, {Routes} from "./routes"
 
   import {
-    TopNavigation,
-    PageHeader,
-    Sidebar,
-    SidebarNavItem,
-    TopNavItem,
-    Loader,
-    UserDropdownMenu,
-    Dropdown,
-    DropdownItem,
-    DropdownButton,
-    DropdownMenu,
-    Label,
-  } from "svelte-adminlte";
+	  login,
+	  isAuthenticated,
+	  userInfo,
+	  AzureProvider,
+	  ZuubrProvider,
+	  appMountCallback,
+	  logout
+  } from "./stores/authentication"
 
-  import MessageLog from "./controls/modals/MessageLog.svelte";
-  import { initSocket } from "./providers/socket";
-  import { each } from "svelte/internal";
-  import SidebarNavTree from "./controls/SidebarNavTree.svelte";
+  import {
+	  TopNavigation,
+	  Sidebar,
+	  SidebarNavItem,
+	  TopNavItem,
+	  Dropdown,
+	  DropdownItem,
+	  DropdownButton,
+	  DropdownMenu
+  } from "svelte-adminlte"
 
-  function applySidebarOpenState() {
-    if (!get(sidebarOpenState)) {
-      document.body.classList.add(
-        "sidebar-collapse",
-        "sidebar-mini-expand-feature"
-      );
-      document.body.classList.remove("sidebar-mini-expand-feature");
-    }
-  }
+  import MessageLog from "./controls/modals/MessageLog.svelte"
+  import {initSocket} from "./providers/socket"
+  import SidebarNavTree from "./controls/SidebarNavTree.svelte"
+  import LocaleDropdown from "./components/LocaleDropdown.svelte"
 
   onMount(() => {
-    // applySidebarOpenState();
+	  initSocket()
 
-    initSocket();
+	  keymage("ctrl-0", () => {
+		  console.log("opening logs")
+		  showLog()
+	  })
+  })
 
-    keymage("ctrl-0", () => {
-      console.log("opening logs");
-      showLog();
-    });
-  });
-
-  let loading = false;
-  let showLog;
-  let localeLanguage = "";
-  const subscription = locale.subscribe((x) => (localeLanguage = x));
+  let loading = false
+  let showLog
+  let localeLanguage = ""
+  const subscription = locale.subscribe((x) => (localeLanguage = x))
   setContext("loader", {
-    setLoading: (val) => (loading = val),
-  });
+	  setLoading: (val) => (loading = val)
+  })
 
-  function changeLanguage(e, lang) {
-    if (lang) {
-      changeLang(lang);
-      location.reload();
-    }
-  }
-
-  onMount(appMountCallback);
+  onMount(appMountCallback)
 </script>
 
 <div class="wrapper">
-  <TopNavigation>
-    <svelte:fragment slot="left">
-      <TopNavItem href="#/">Home</TopNavItem>
-      <Dropdown>
-        <DropdownButton>Pages</DropdownButton>
+	<TopNavigation>
+		<svelte:fragment slot="left">
+			<TopNavItem href="#/">Home</TopNavItem>
+			<Dropdown>
+				<DropdownButton>Pages</DropdownButton>
 
-        <DropdownMenu>
-          <DropdownItem href="#/list">List</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </svelte:fragment>
+				<DropdownMenu>
+					<DropdownItem href="#/list">List</DropdownItem>
+				</DropdownMenu>
+			</Dropdown>
+		</svelte:fragment>
 
-    <svelte:fragment slot="right">
-      <Dropdown>
-        <DropdownButton
-          ><img
-            src={GetFlagPath(localeLanguage)}
-            alt={localeLanguage}
-          /></DropdownButton
-        >
-        <div id="language-dropdown">
-          <DropdownMenu right>
-            {#each languages as l}
-              <div
-                class="lang-item"
-                on:click={(e) => {
-                  changeLanguage(e, l.code);
-                }}
-              >
-                <img src={GetFlagPath(l.code)} alt={l.img} />
-                {l.title || l.code}
-              </div>
-            {/each}
-          </DropdownMenu>
-        </div>
-      </Dropdown>
-      {#if $isAuthenticated}
-        <Dropdown slot="right">
-          <DropdownButton>{$userInfo.name}</DropdownButton>
+		<svelte:fragment slot="right">
+			<LocaleDropdown />
 
-          <DropdownMenu right>
-            <DropdownItem on:click={() => logout()}>Log Out</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      {:else}
-        <Dropdown>
-          <DropdownButton>Log In</DropdownButton>
-          <DropdownMenu right>
-            <DropdownItem on:click={() => login(AzureProvider)}
-              >Azure</DropdownItem
-            >
-            <DropdownItem on:click={() => login(ZuubrProvider)}
-              >Zuubr</DropdownItem
-            >
-          </DropdownMenu>
-        </Dropdown>
-      {/if}
-    </svelte:fragment>
-  </TopNavigation>
+			{#if $isAuthenticated}
+				<Dropdown slot="right">
+					<DropdownButton>{$userInfo.name}</DropdownButton>
 
-  <Sidebar>
-    {#each Routes as route}
-      {#if !route.hide}
-        {#if route.nesting}
-          <SidebarNavTree icon={route.icon} href="#{route.route}"
-            >{route.title}
-            <svelte:fragment slot="children">
-              {#each route.subroutes as sub}
-                <SidebarNavItem icon={sub.icon} href="#{sub.route}"> 
-                  <p>{sub.title}</p>
-                </SidebarNavItem>
-              {/each}
-            </svelte:fragment>
-          </SidebarNavTree>
-        {:else}
-          <SidebarNavItem icon={route.icon} href="#{route.route}">
-            <p>{route.title}</p>
-          </SidebarNavItem>
-        {/if}
-      {/if}
-    {/each}
-  </Sidebar>
+					<DropdownMenu right>
+						<DropdownItem on:click={() => logout()}>Log Out</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			{:else}
+				<Dropdown>
+					<DropdownButton>Log In</DropdownButton>
+					<DropdownMenu right>
+						<DropdownItem on:click={() => login(AzureProvider)}
+						>Azure
+						</DropdownItem
+						>
+						<DropdownItem on:click={() => login(ZuubrProvider)}
+						>Zuubr
+						</DropdownItem
+						>
+					</DropdownMenu>
+				</Dropdown>
+			{/if}
+		</svelte:fragment>
+	</TopNavigation>
 
-  <div class="content-wrapper">
-    <!-- {#if loading}
-      <Loader />
-    {/if} -->
-    <!-- <PageHeader /> -->
-    <div class="content">
-      <Router {routes} />
-    </div>
-  </div>
+	<Sidebar>
+		{#each Routes as route}
+			{#if !route.hide}
+				{#if route.nesting}
+					<SidebarNavTree icon={route.icon} href="#{route.route}"
+					>{route.title}
+						<svelte:fragment slot="children">
+							{#each route.subroutes as sub}
+								<SidebarNavItem icon={sub.icon} href="#{sub.route}">
+									<p>{sub.title}</p>
+								</SidebarNavItem>
+							{/each}
+						</svelte:fragment>
+					</SidebarNavTree>
+				{:else}
+					<SidebarNavItem icon={route.icon} href="#{route.route}">
+						<p>{route.title}</p>
+					</SidebarNavItem>
+				{/if}
+			{/if}
+		{/each}
+	</Sidebar>
 
-  <MessageLog bind:show={showLog} />
+	<div class="content-wrapper">
+		<!-- {#if loading}
+			<Loader />
+		{/if} -->
+		<!-- <PageHeader /> -->
+		<div class="content">
+			<Router {routes} />
+		</div>
+	</div>
+
+	<MessageLog bind:show={showLog} />
 </div>
-
-<style lang="sass">
-:global
-  #language-dropdown 
-    .dropdown-menu
-      min-width: 0
-.lang-item
-    cursor: pointer
-    white-space: nowrap
-    padding: 0 1rem
-</style>
--- assets/src/locale/i18n.js
+++ assets/src/locale/i18n.js
@@ -1,72 +1,72 @@
-import { init, getLocaleFromNavigator, locale, addMessages, json,register } from "svelte-i18n";
+import {init, getLocaleFromNavigator, locale, addMessages, json, register} from "svelte-i18n"
 
-import en from "./en.json";
-import cs from "./cs.json";
-import langs from "./langs.json";
-import { registerLocaleLoader } from "svelte-i18n/types/runtime/includes/loaderQueue";
-
-
-export { locale, locales, langs };
-export const languages = langs;
-
-let locales  ={"cs":cs,"en":en}
-inicialize();
+import en from "./en.json"
+import cs from "./cs.json"
+import langs from "./langs.json"
+import {registerLocaleLoader} from "svelte-i18n/types/runtime/includes/loaderQueue"
 
 
+export {locale, locales, langs}
+export const languages = langs
 
-function inicialize(){
-  langs.forEach((lang) => {
-    let lc = localStorage.getItem(lang.code + "-locale");
-    if (lc != null) {
-      console.log("loading from ls" + lang.code)
-      addMessages(lang.code, JSON.parse(lc));
-      locales[lang.code] = JSON.parse(lc);
-    }else{
-      addMessages(lang.code,locales[lang.code]);
-    }
-  });
+let locales = {"cs": cs, "en": en}
+initialize()
 
-  init({
-    fallbackLocale: "cs",
-    initialLocale: localStorage.getItem("language") || getLocaleFromNavigator(),
-  });
-  
+
+function initialize() {
+	langs.forEach((lang) => {
+		let lc = localStorage.getItem(lang.code + "-locale")
+		if (lc != null) {
+			console.log("loading from ls" + lang.code)
+			addMessages(lang.code, JSON.parse(lc))
+			locales[lang.code] = JSON.parse(lc)
+		} else {
+			addMessages(lang.code, locales[lang.code])
+		}
+	})
+
+	init({
+		fallbackLocale: "cs",
+		initialLocale: localStorage.getItem("language") || getLocaleFromNavigator()
+	})
+
 }
-export function GetFlagPath(country_code) {
-  let lang = languages.find((x) => x.code === country_code.substring(0, 2));
-  if (lang != undefined) return "img/flags/" + lang.img + ".png";
-  return "";
+
+export function getFlagPath(countryCode) {
+	let lang = languages.find((x) => x.code === countryCode.substring(0, 2))
+	return lang
+		? "img/flags/" + lang.img + ".png"
+		: ""
 }
 
 export function saveLanguageFile(json, lang) {
-  addMessages(lang, json);
-  localStorage.setItem(lang + "-locale", JSON.stringify(json));
-  
+	addMessages(lang, json)
+	localStorage.setItem(lang + "-locale", JSON.stringify(json))
+
 }
 
 export function deleteSaveLocals() {
 
-  let keys,i;
-  keys = Object.keys(localStorage);
-  i = keys.length;
+	let keys, i
+	keys = Object.keys(localStorage)
+	i = keys.length
 
-  while (i--) {
-    if(keys[i].substring(3) === "locale"){
-      localStorage.removeItem(keys[i])
-    }
-  }
+	while (i--) {
+		if (keys[i].substring(3) === "locale") {
+			localStorage.removeItem(keys[i])
+		}
+	}
 
-  console.log("deleting saved locals")
-  location.reload();
+	console.log("deleting saved locals")
+	location.reload()
 }
 
 export function changeLang(lang) {
-  if (languages.find((x) => x.code === lang.substring(0, 2)) != undefined) {
-    console.log("changing lang to:", lang);
-    locale.set(lang);
-    localStorage.setItem("language", lang);
-  } else {
-    console.log("ERROR: language " + lang, " does not exist");
-  }
-  return;
+	if (languages.find((x) => x.code === lang.substring(0, 2))) {
+		console.log("changing lang to:", lang)
+		locale.set(lang)
+		localStorage.setItem("language", lang)
+	} else {
+		console.log("ERROR: language " + lang, " does not exist")
+	}
 }

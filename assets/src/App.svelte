895595ba41db69dd4b2c2ced66b535323761a587
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-04-27 19:11:11 +0200

    added toggle button for condensed

--- assets/src/App.svelte
+++ assets/src/App.svelte
@@ -1,177 +1,199 @@
 <script>
-  import { onMount, setContext } from "svelte";
-  import { get } from "svelte/store";
-  import Router from "svelte-spa-router";
-  import keymage from "keymage";
-  import "./locale/i18n";
-  import { locale } from "./locale/i18n";
-  import routes, { Routes } from "./routes";
-  import {
-    login,
-    isAuthenticated,
-    userInfo,
-    AzureProvider,
-    ZuubrProvider,
-    appMountCallback,
-    logout,
-  } from "./stores/authentication";
-  import {
-    TopNavigation,
-    Sidebar,
-    SidebarNavItem,
-    TopNavItem,
-    Dropdown,
-    DropdownItem,
-    DropdownButton,
-    DropdownMenu,
-  } from "svelte-adminlte";
-  import MessageLog from "./components/modals/MessageLog.svelte";
-  import { initSocket } from "./providers/socket";
-  import SidebarNavTree from "./components/SidebarNavTree.svelte";
-  import LocaleDropdown from "./components/localEditor/LocaleDropdown.svelte";
-  import { _ } from "svelte-i18n";
-  import jq from "jquery";
+	import { onMount, setContext } from "svelte";
+	import { get } from "svelte/store";
+	import Router from "svelte-spa-router";
+	import keymage from "keymage";
+	import "./locale/i18n";
+	import { locale } from "./locale/i18n";
+	import routes, { Routes } from "./routes";
+	import {
+		login,
+		isAuthenticated,
+		userInfo,
+		AzureProvider,
+		ZuubrProvider,
+		appMountCallback,
+		logout,
+	} from "./stores/authentication";
+	import {
+		TopNavigation,
+		Sidebar,
+		SidebarNavItem,
+		TopNavItem,
+		Dropdown,
+		DropdownItem,
+		DropdownButton,
+		DropdownMenu,
+		Checkbox,
+		Label,
+	} from "svelte-adminlte";
+	import MessageLog from "./components/modals/MessageLog.svelte";
+	import { initSocket } from "./providers/socket";
+	import SidebarNavTree from "./components/SidebarNavTree.svelte";
+	import LocaleDropdown from "./components/localEditor/LocaleDropdown.svelte";
+	import { _ } from "svelte-i18n";
+	import jq from "jquery";
 
-  import { AppUrl } from './constants/urls';
+	import { AppUrl } from "./constants/urls";
 
-  import SvelteSelect from "svelte-select";
-  onMount(() => {
-    initSocket();
-    keymage("ctrl-0", () => {
-      console.log("opening logs");
-      showLog();
-    });
-  });
-  let loading = false;
-  let showLog;
-  let localeLanguage = "";
-  const subscription = locale.subscribe((x) => (localeLanguage = x));
-  setContext("loader", {
-    setLoading: (val) => (loading = val),
-  });
-  onMount(appMountCallback);
+	import SvelteSelect from "svelte-select";
+	import FormGroup from "svelte-adminlte/src/form/structure/FormGroup.svelte";
+	onMount(() => {
+		initSocket();
+		keymage("ctrl-0", () => {
+			console.log("opening logs");
+			showLog();
+		});
+	});
+	let loading = false;
+	let showLog;
+	let localeLanguage = "";
+	const subscription = locale.subscribe((x) => (localeLanguage = x));
+	setContext("loader", {
+		setLoading: (val) => (loading = val),
+	});
+	onMount(appMountCallback);
 
-  let fonts = [
-    {
-      label: "Source Sans Pro",
-      value:
-        '"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-    },
-    { label: "Report", value: "Report" },
-    { label: "Assailand", value: "Assailand" },
-    { label: "Iunito", value: "Iunito" },
-    { label: "Codec Pro", value: "Codec Pro" },
-    { label: "Betinya Sans", value: "Betinya Sans" },
-    { label: "Acephimere", value: "Acephimere" },
-    { label: "Tepeno Sans", value: "Tepeno Sans" },
-    { label: "Homizio Nova", value: "Homizio Nova" },
-  ];
+	let fonts = [
+		{
+			label: "Source Sans Pro",
+			value:
+				'"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+		},
+		{ label: "Report", value: "Report" },
+		{ label: "Assailand", value: "Assailand" },
+		{ label: "Iunito", value: "Iunito" },
+		{ label: "Codec Pro", value: "Codec Pro" },
+		{ label: "Betinya Sans", value: "Betinya Sans" },
+		{ label: "Acephimere", value: "Acephimere" },
+		{ label: "Tepeno Sans", value: "Tepeno Sans" },
+		{ label: "Homizio Nova", value: "Homizio Nova" },
+	];
 
-  function changeFont(ev) {
-    console.log(ev.detail.value);
-    document.querySelector("body").style.fontFamily = ev.detail.value;
-  }
-  jq(function () {
-    jq('[data-toggle="tooltip"]').tooltip({delay: { "show": 800, "hide": 0 }});
-  });
+	function changeFont(ev) {
+		console.log(ev.detail.value);
+		document.querySelector("body").style.fontFamily = ev.detail.value;
+	}
+	jq(function () {
+		jq('[data-toggle="tooltip"]').tooltip({ delay: { show: 800, hide: 0 } });
+	});
+
+	let condensed = true;
+	$: setCondensed(condensed);
+	function setCondensed(val) {
+		if (val) {
+			document.body.classList.add("condensed");
+		} else {
+			document.body.classList.remove("condensed");
+		}
+	}
 </script>
 
 <div class="wrapper">
-  <TopNavigation>
-    <svelte:fragment slot="left">
-      <TopNavItem href="#/">Home</TopNavItem>
-      <Dropdown>
-        <DropdownButton>Pages</DropdownButton>
+	<TopNavigation>
+		<svelte:fragment slot="left">
+			<TopNavItem href="#/">Home</TopNavItem>
+			<Dropdown>
+				<DropdownButton>Pages</DropdownButton>
 
-        <DropdownMenu>
-          {#each Routes as route}
-            {#if !route.hide}
-              <DropdownItem href={"#" + route.route}>{route.title}</DropdownItem
-              >
-            {/if}
-          {/each}
-        </DropdownMenu>
-      </Dropdown>
-      <TopNavItem >
-        {#if AppUrl.startsWith("http://l")}
-        <span class="text-danger font-weight-bold">LOCALHOST</span>
-        {:else}
-          { AppUrl.startsWith("https://d") ? "DEV" : "MAIN"}
-        {/if}
-        </TopNavItem>
-    </svelte:fragment>
+				<DropdownMenu>
+					{#each Routes as route}
+						{#if !route.hide}
+							<DropdownItem href={"#" + route.route}>{route.title}</DropdownItem
+							>
+						{/if}
+					{/each}
+				</DropdownMenu>
+			</Dropdown>
+			<TopNavItem>
+				{#if AppUrl.startsWith("http://l")}
+					<span class="text-danger font-weight-bold">LOCALHOST</span>
+				{:else}
+					{AppUrl.startsWith("https://d") ? "DEV" : "MAIN"}
+				{/if}
+			</TopNavItem>
+		</svelte:fragment>
 
-    <svelte:fragment slot="right">
-      <SvelteSelect
-        items={fonts}
-        on:select={changeFont}
-        value={fonts[0]}
-        containerClasses="minWidth"
-      />
-      <LocaleDropdown />
-      {#if $isAuthenticated}
-        <Dropdown slot="right">
-          <DropdownButton>{$userInfo.name}</DropdownButton>
+		<svelte:fragment slot="right">
+			<FormGroup>
+				<Checkbox bind:checked={condensed} id="condensed"
+					><Label inputId="condensed">condensed</Label></Checkbox
+				>
+			</FormGroup>
 
-          <DropdownMenu right>
-            <DropdownItem on:click={() => logout()}>Log Out</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      {:else}
-        <Dropdown>
-          <DropdownButton>Log In</DropdownButton>
-          <DropdownMenu right>
-            <DropdownItem on:click={() => login(AzureProvider)}
-              >Azure
-            </DropdownItem>
-            <DropdownItem on:click={() => login(ZuubrProvider)}
-              >Zuubr
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      {/if}
-    </svelte:fragment>
-  </TopNavigation>
+			<SvelteSelect
+				items={fonts}
+				on:select={changeFont}
+				value={fonts[0]}
+				containerClasses="minWidth"
+			/>
+			<LocaleDropdown />
+			{#if $isAuthenticated}
+				<Dropdown slot="right">
+					<DropdownButton>{$userInfo.name}</DropdownButton>
 
-  <Sidebar>
-    {#each Routes as route}
-      {#if !route.hide}
-        {#if route.nesting}
-          <SidebarNavTree icon={route.icon} href="#{route.route}"
-            >{$_("routes." + route.name + ".navtitle", {
-              default: route.title,
-            })}
-            <svelte:fragment slot="children">
-              {#each route.subroutes as sub}
-                <SidebarNavItem icon={sub.icon} href="#{sub.route}">
-                  <p>{sub.title}</p>
-                </SidebarNavItem>
-              {/each}
-            </svelte:fragment>
-          </SidebarNavTree>
-        {:else}
-          <SidebarNavItem icon={route.icon} href="#{route.route}" 
-          tooltip= {$_("routes." + route.name + ".tooltip", {default: ""})}>
-            <p>
-              {$_("routes." + route.name + ".navtitle", {
-                default: route.title,
-              })}
-            </p>
-          </SidebarNavItem>
-        {/if}
-      {/if}
-    {/each}
-  </Sidebar>
+					<DropdownMenu right>
+						<DropdownItem on:click={() => logout()}>Log Out</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			{:else}
+				<Dropdown>
+					<DropdownButton>Log In</DropdownButton>
+					<DropdownMenu right>
+						<DropdownItem on:click={() => login(AzureProvider)}
+							>Azure
+						</DropdownItem>
+						<DropdownItem on:click={() => login(ZuubrProvider)}
+							>Zuubr
+						</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			{/if}
+		</svelte:fragment>
+	</TopNavigation>
 
-  <div class="content-wrapper">
-    <!-- {#if loading}
+	<Sidebar>
+		{#each Routes as route}
+			{#if !route.hide}
+				{#if route.nesting}
+					<SidebarNavTree icon={route.icon} href="#{route.route}"
+						>{$_("routes." + route.name + ".navtitle", {
+							default: route.title,
+						})}
+						<svelte:fragment slot="children">
+							{#each route.subroutes as sub}
+								<SidebarNavItem icon={sub.icon} href="#{sub.route}">
+									<p>{sub.title}</p>
+								</SidebarNavItem>
+							{/each}
+						</svelte:fragment>
+					</SidebarNavTree>
+				{:else}
+					<SidebarNavItem
+						icon={route.icon}
+						href="#{route.route}"
+						tooltip={$_("routes." + route.name + ".tooltip", { default: "" })}
+					>
+						<p>
+							{$_("routes." + route.name + ".navtitle", {
+								default: route.title,
+							})}
+						</p>
+					</SidebarNavItem>
+				{/if}
+			{/if}
+		{/each}
+	</Sidebar>
+
+	<div class="content-wrapper">
+		<!-- {#if loading}
 			<Loader />
 		{/if} -->
-    <!-- <PageHeader /> -->
-    <div class="content">
-      <Router {routes} />
-    </div>
-  </div>
+		<!-- <PageHeader /> -->
+		<div class="content">
+			<Router {routes} />
+		</div>
+	</div>
 
-  <MessageLog bind:show={showLog} />
+	<MessageLog bind:show={showLog} />
 </div>

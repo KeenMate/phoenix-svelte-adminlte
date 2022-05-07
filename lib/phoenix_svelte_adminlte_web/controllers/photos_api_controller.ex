72fb30693bb420a2b1e7d2cc87f5af5ccb3b4263
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-05-07 21:51:36 +0200

    image upload works

--- lib/phoenix_svelte_adminlte_web/controllers/photos_api_controller.ex
+++ lib/phoenix_svelte_adminlte_web/controllers/photos_api_controller.ex
@@ -2,6 +2,7 @@ defmodule PhoenixSvelteAdminlteWeb.PhotosApiController do
   use PhoenixSvelteAdminlteWeb, :controller
   require Logger
   alias PhoenixSvelteAdminlte.Image.Manager, as: ImageManager
+  alias PhoenixSvelteAdminlte.Image.Helpers
   # alias PhoenixSvelteAdminlteWeb.Router.Helpers, as: WebRoutes
 
   def get(conn, _params) do
@@ -24,6 +25,7 @@ defmodule PhoenixSvelteAdminlteWeb.PhotosApiController do
   end
 
   defp inscribe_image_urls(images, _conn) do
+    # TODO make real routes
     images
     |> Enum.map(fn %{uuid: uuid} = image ->
       image
@@ -33,5 +35,45 @@ defmodule PhoenixSvelteAdminlteWeb.PhotosApiController do
       |> Map.put(:original_url, "https://picsum.photos/id/#{uuid}/1920/1080")
       |> Map.put(:thumbnail_url, "https://picsum.photos/id/#{uuid}/400/400")
     end)
+  end
+
+  def process(
+        conn,
+        %{"filepond" => %Plug.Upload{} = image}
+      ) do
+    Logger.info("Received image to be processed: #{inspect(image)}")
+
+    uuid = Helpers.generate_uuid()
+    result = PhoenixSvelteAdminlte.Image.Manager.save_image(image.path, uuid, image.content_type)
+
+    # TODO uncomment
+    # Logger.debug("Registering image for Queue dispatch")
+    # PhotoQueueDispatcher.register(image.filename, uuid)
+
+    case result do
+      :ok -> send_resp(conn, 200, "")
+      :error -> send_resp(conn, 500, "Error saving image")
+    end
+  end
+
+  def process(
+        conn,
+        %{
+          "upload" => %{
+            "images" => [_ | [%Plug.Upload{} = image | _]]
+          }
+        }
+      ) do
+    uuid = Helpers.generate_uuid()
+    result = PhoenixSvelteAdminlte.Image.Manager.save_image(image.path, uuid, image.content_type)
+
+    # TODO uncomment
+    # Logger.debug("Registering image for Queue dispatch")
+    # PhotoQueueDispatcher.register(image.filename, uuid)
+
+    case result do
+      :ok -> send_resp(conn, 200, "")
+      :error -> send_resp(conn, 500, "Error saving image")
+    end
   end
 end

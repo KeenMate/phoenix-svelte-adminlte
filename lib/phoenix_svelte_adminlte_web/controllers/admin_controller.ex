700f88699c9a0c8be7ab7645f58654fb34351b51
Albert Moravec <albert.moravec@gmail.com>
2022-08-10 02:38:24 +0200

    Redirect to login with redirect properly

--- lib/phoenix_svelte_adminlte_web/controllers/admin_controller.ex
+++ lib/phoenix_svelte_adminlte_web/controllers/admin_controller.ex
@@ -6,9 +6,13 @@ defmodule PhoenixSvelteAdminlteWeb.AdminController do
 
   import Authorize
 
-  plug RequireAuthenticated, storage: KeenAuth.Config.get_storage(), login_path: Application.get_env(:keen_auth, :login_path)
+  plug RequireAuthenticated, storage: KeenAuth.Config.get_storage(), login_path: &PhoenixSvelteAdminlteWeb.AdminController.login_path/2
 
   plug :roles, storage: KeenAuth.Config.get_storage(), op: :and, roles: ["admin"], only: [:secret]
+
+  def login_path(conn, redirect_to) do
+    Routes.auth_path(conn, :new, :aad, redirect_to: redirect_to)
+  end
 
   def secret(conn, _params) do
     conn
